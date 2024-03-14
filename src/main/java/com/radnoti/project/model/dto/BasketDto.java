3b5f6099ba45ba508de962d2c35c2acd17395721
FiloZsofia <68869637+FiloZsofia@users.noreply.github.com>
2024-03-14 17:51:42 +0100

    Basket entity and DTO

---
+++ src/main/java/com/radnoti/project/model/dto/BasketDto.java
@@ -0,0 +1,16 @@
+package com.radnoti.project.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BasketDto {
+    private Integer id;
+    private Integer user_id;
+    private Integer art_id;
+}
